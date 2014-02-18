87e4c9cc9a73e385e884e4c3aa0a09bb67452258
Lesley De Cruz <lesley.decruz+git@gmail.com>
2014-02-18 22:13:56 +0100

    Wrap the solutions for y and dydt in a column matrix when indexing the state.

--- benchmarks/ode/ode-benchmark-rk8pd-vec.lua
+++ benchmarks/ode/ode-benchmark-rk8pd-vec.lua
@@ -21,11 +21,11 @@ local y0 = matrix.vec {1, 0}
 local t0, t1, h0 = 0, 20000, 0.01
 local evol = s.evolve
 for k=1, 10 do
-   s:init(t0, h0, f, y0.data)
+   s:init(t0, h0, f, y0)
    while s.t < t1 do
       evol(s, f, t1)
    end
-   results[#results+1] = format('%g %g %g', s.t, s.y[0], s.y[1])
+   results[#results+1] = format('%g %g %g', s.t, s.y[1], s.y[2])
 end
 
 for i, line in ipairs(results) do
